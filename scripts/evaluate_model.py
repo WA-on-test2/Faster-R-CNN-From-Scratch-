e6714de3b8ae711132e3eb0b7e710467e8b8ccfe
WA-on-test2 <marina.safwat2002@gmail.com>
2025-12-17 19:55:16 +0200

    ****Evaluation Script****

---
+++ scripts/evaluate_model.py
@@ -0,0 +1,38 @@
+import argparse
+import torch
+from torch.utils.data import DataLoader
+
+from config.base_config import load_configuration
+from core.models.detector import ObjectDetectionModel
+from data.loaders.voc_loader import VOCDatasetLoader
+from controllers.evaluator import ModelEvaluator
+from utils.common import get_computation_device
+
+def main():
+    parser = argparse.ArgumentParser(description='Evaluate object detection model')
+    parser.add_argument('--config', type=str, default='config/voc_dataset.yaml')
+    args = parser.parse_args()
+    
+    config = load_configuration(args.config)
+    device = get_computation_device()
+    
+    test_dataset = VOCDatasetLoader(
+        'test',
+        config['dataset_params']['im_test_path'],
+        config['dataset_params']['ann_test_path'])
+    
+    test_loader = DataLoader(test_dataset, batch_size=1, shuffle=False)
+    
+    model = ObjectDetectionModel(
+        config['model_params'],
+        num_classes=config['dataset_params']['num_classes'])
+    
+    checkpoint_path = f"{config['train_params']['task_name']}/{config['train_params']['ckpt_name']}"
+    model.load_state_dict(torch.load(checkpoint_path, map_location=device))
+    model.eval()
+    model.to(device)
+    evaluator = ModelEvaluator(model, test_loader, test_dataset)
+    evaluator.evaluate()
+
+if __name__ == '__main__':
+    main()
